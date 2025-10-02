f07ad0540c7f97b80cc690bef5384e95df57075b
SCHANDAN996 <work.chandansingh@gmail.com>
2025-10-02 20:22:24 +0530

    Delete passenger_wsgi.py

--- passenger_wsgi.py
+++
@@ -1,5 +0,0 @@
-# Filepath: passenger_wsgi.py
-
-# This file is the entry point for Hostinger's Python App setup.
-# It simply imports the 'app' object from your main run file.
-from run import app as application
