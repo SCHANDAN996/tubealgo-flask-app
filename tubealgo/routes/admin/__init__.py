42af2c2eb90c4db7ab754083f9fafc4521eac817
SCHANDAN996 <work.chandansingh@gmail.com>
2025-10-25 14:13:00 +0530

    Update __init__.py

--- tubealgo/routes/admin/__init__.py
+++ tubealgo/routes/admin/__init__.py
@@ -1,17 +1,11 @@
 # tubealgo/routes/admin/__init__.py
 
 from flask import Blueprint
-# from flask_wtf.csrf import generate_csrf # <<<--- यह इम्पोर्ट हटाएं
 
-# मुख्य एडमिन Blueprint यहाँ बनाया गया है
+# Create main admin blueprint
 admin_bp = Blueprint('admin', __name__, template_folder='../../templates/admin')
 
-# <<<--- कॉन्टेक्स्ट प्रोसेसर यहाँ से हटा दिया गया है --->>>
-# @admin_bp.context_processor
-# def inject_csrf_token_admin():
-#    """Injects the CSRF token generation function into admin templates."""
-#    return dict(csrf_token=generate_csrf)
-# <<<--- यहाँ तक हटाया गया है --->>>
-
-# बाकी की एडमिन route फाइलों को इम्पोर्ट करें ताकि उनके रूट्स रजिस्टर हो जाएं
+# Import sub-modules AFTER blueprint creation to avoid circular imports
 from . import dashboard, users, monetization, system
+
+# All routes are now registered through the sub-modules
