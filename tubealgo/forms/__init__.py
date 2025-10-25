aa3f31a87d86913b14c8f2b11d1470bc2e2aee01
SCHANDAN996 <work.chandansingh@gmail.com>
2025-10-25 10:55:12 +0530

    Update __init__.py

--- tubealgo/forms/__init__.py
+++ tubealgo/forms/__init__.py
@@ -2,10 +2,12 @@
 
 from .auth_forms import SignupForm, LoginForm
 from .youtube_forms import PlaylistForm, VideoForm, UploadForm
-from .admin_forms import CouponForm, PlanForm
+# <<< बदलाव यहाँ है: PlanForm को SubscriptionPlanForm से बदला गया >>>
+from .admin_forms import CouponForm, SubscriptionPlanForm
 
 __all__ = [
     "SignupForm", "LoginForm",
     "PlaylistForm", "VideoForm", "UploadForm",
-    "CouponForm", "PlanForm"
+    # <<< बदलाव यहाँ है: PlanForm को SubscriptionPlanForm से बदला गया >>>
+    "CouponForm", "SubscriptionPlanForm"
 ]
